c72fb492b46861c2f515a998047470b8f7adbf76
kamaoji <karun73364@gmail.com>
2025-03-08 14:39:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,20 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+export default {
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // सभी node_modules अलग फाइल में रहेंगे
+          }
+        }
+      }
+    }
+  }
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -19,17 +33,3 @@ export default defineConfig({
   //   },
   // },
 });
-
-export default {
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // सभी node_modules अलग फाइल में रहेंगे
-          }
-        }
-      }
-    }
-  }
-};
